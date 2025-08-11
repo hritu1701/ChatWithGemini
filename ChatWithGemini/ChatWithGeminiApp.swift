13473085e2aa6f4f84e078deb3426c433a4f6e42
Hritu Raj <sac3xhin@gmail.com>
2025-08-11 20:07:16 +0530

    Initial Commit

---
+++ ChatWithGemini/ChatWithGeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithGeminiApp.swift
+//  ChatWithGemini
+//
+//  Created by Hritu Raj on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithGeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
